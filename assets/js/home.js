43716453d31bb2fdcef6a96f3ea7f88e006c44c4
juldossmann <jul.dossmann@gmail.com>
2020-03-02 15:32:01 +0000

    Ajout du bouton de retour en haut de page

---
+++ assets/js/home.js
@@ -0,0 +1,15 @@
+console.log("salut ma yecou");
+
+
+window.addEventListener("DOMContentLoaded", function() {
+    let mybutton = document.getElementById("myBtn");
+    mybutton.addEventListener("click", function(){
+            
+
+
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    
+    })
+  });
+
