a6e2e6f88bf004a5ae4d5f07ceb866944ef78cc8
juldossmann <jul.dossmann@gmail.com>
2020-03-05 10:14:21 +0000

    Implémentation des boutons de changement de langue français englais et script correspondant. Résolution du bug créé par les promotions si la bdd est vide

--- assets/js/home.js
+++ assets/js/home.js
@@ -13,3 +13,36 @@ window.addEventListener("DOMContentLoaded", function() {
     })
   });
 
+
+
+  window.addEventListener("DOMContentLoaded", function() {
+    let frenchButton = document.getElementById("frenchButton");
+    let isFrench = true;
+    let englishButton = document.getElementById("englishButton");
+    let isEnglish = false;
+    englishButton.addEventListener("click", function(){
+            
+        if(isEnglish == false){
+            isEnglish = true;
+            isFrench = false;
+            console.log("isEnglish = " + isEnglish);
+            console.log("isFrench = " + isFrench);
+        }else{
+            alert("This website is already in english");
+        }
+    
+    })
+
+
+    frenchButton.addEventListener("click", function(){
+    
+        if(isFrench == false){
+            isEnglish = false;
+            isFrench = true;
+            console.log("isFrench = " + isFrench);
+            console.log("isEnglish = " + isEnglish);
+        }else{
+            alert("Le site est déjà en français");
+        }
+    })
+  });
